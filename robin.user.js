abba7becf924c3b713bfe1adf44555bc167c6b84
Alberto <mail4alberto@gmail.com>
2016-04-07 05:13:12 -0400

    v3.56

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         parrot (color multichat for robin!)
 // @namespace    http://tampermonkey.net/
-// @version      3.55
+// @version      3.56
 // @description  Recreate Slack on top of an 8 day Reddit project.
 // @author       dashed, voltaek, daegalus, vvvv, orangeredstilton, lost_penguin, AviN456, Annon201
 // @include      https://www.reddit.com/robin*
@@ -1696,6 +1696,8 @@
     // Send message button
     $("#robinSendMessage").append('<div onclick={$(".text-counter-input").submit();} class="robin-chat--vote" id="sendBtn">Send Message</div>'); // Send message
     $("#robinSendMessage").on("submit", function() { onMessageBoxSubmit(); } );
+
+    $("#sendBtn").on("click", function() { onMessageBoxSubmit(); } );
 
     // Setup page for tabbed channels
     setupMultiChannel();
