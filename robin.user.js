32bbe72423d19ddd0c392c218407a772abf0f7f5
mofosyne <mofosyne@gmail.com>
2016-04-08 02:00:29 +1000

    This is the logic for channel discovery no display yet (#161)
    * start of a channel discovery thing - Now needs a way to do display
    
    Written a different word count but opted to just use
    http://stackoverflow.com/questions/16742726/sorting-an-array-of-data-on-frequency-of-occurence
    to do the counting. They write it better.
    
    * channel discovery histories pruning for not killing the ram, and also to keep it more recent to 100 messages
    
    * removing diff fluff

--- robin.user.js
+++ robin.user.js
@@ -18,6 +18,7 @@
     var CURRENT_CHANNEL = "";
     var GOTO_BOTTOM = true;
     var robinChatWindow = $('#robinChatWindow');
+    var channelDiscoveryList = []; // This will be used to store the first word of each incoming message to discovery new channels
 
     String.prototype.lpad = function(padString, length) {
         var str = this;
@@ -1398,6 +1399,35 @@
                 var thisUser = $user.text();
                 var $message = $(jq[0]).find('.robin-message--message');
                 var messageText = $message.text();
+
+                // Channel Discovery - /u/mofosyne
+                var tokenisedMsg = messageText.split(" ");
+                var channelName = tokenisedMsg[0];
+                // We want to save only those that doesn't look like spam
+                var channelNameRegExp = new RegExp("^([!@#$%\^&*()]+)(.?)+$");
+                if( channelNameRegExp.test(channelName) && channelName != ""){
+                    channelDiscoveryList.push(channelName); // This push it to our 'history log' of potential channel names
+                    // To avoid killing the ram, we should pop old entries after it reaches 100
+                    if (channelDiscoveryList.length > 100){
+                        channelDiscoveryList.shift();
+                    }
+                    // Find most common channel name http://stackoverflow.com/questions/16742726/sorting-an-array-of-data-on-frequency-of-occurence
+                    function orderByOccurrence(arr) {
+                        var counts = {};
+                        arr.forEach(function(value){
+                            if(!counts[value]) {
+                                counts[value] = 0;
+                            }
+                            counts[value]++;
+                        });
+                        return Object.keys(counts).sort(function(curKey,nextKey) {
+                            return counts[curKey] < counts[nextKey];
+                        });
+                    }
+                    console.log(" top 5 channel: "+orderByOccurrence(channelDiscoveryList).slice(0, 5));
+                    //console.log(" channelDiscoveryList: "+channelDiscoveryList);
+                }
+
 
                 // Decryption
                 var chanName = hasChannel(messageText).name;
