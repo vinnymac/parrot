10d72ebd54e6d07da6d63473531098a297e93194
ZeWaka <wakafanykai123+git@gmail.com>
2016-04-07 07:03:00 -1000

    3.64

--- robin.user.js
+++ robin.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         parrot (color multichat for robin!)
 // @namespace    http://tampermonkey.net/
-// @version      3.63
+// @version      3.64
 // @description  Recreate Slack on top of an 8 day Reddit project.
 // @author       dashed, voltaek, daegalus, vvvv, orangeredstilton, lost_penguin, AviN456, Annon201, LTAcosta, mofosyne
 // @include      https://www.reddit.com/robin*
